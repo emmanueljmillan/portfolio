2433d69cca2066a0dc9497a7e783b8b716b6d165
Emmanuel Millan <emmanueljmillan@outlook.com>
2026-06-04 23:54:16 -0400

    Small change

--- src/pages/project-pages/genomic-sequence-modeling.jsx
+++ src/pages/project-pages/genomic-sequence-modeling.jsx
@@ -1,5 +1,4 @@
 import Dither from "../../components/Dither"
-import PixelTrail from "../../components/PixelTrail"
 
 export default function GenomicSequencing() {
   return (
@@ -14,20 +13,6 @@ export default function GenomicSequencing() {
           waveAmplitude={0.5}
           waveFrequency={6}
           waveSpeed={0.015}
-        />
-      </div>
-
-      <div className="project-page-pixel-layer">
-        <PixelTrail
-          gradientColors={["#5227FF", "#dd94df", "#e9455b"]}
-          gridSize={100}
-          trailSize={0.015}
-          maxAge={150}
-          interpolate={2}
-          color="#5227FF"
-          gooeyFilter={{ id: "project-page-goo-filter", strength: 2 }}
-          gooeyEnabled
-          gooStrength={2}
         />
       </div>
 
